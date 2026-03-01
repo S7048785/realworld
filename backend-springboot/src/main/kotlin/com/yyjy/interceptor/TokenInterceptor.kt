5125addb6b7682d954eba3d9b374864bc911484b
S7048785 <141379079+S7048785@users.noreply.github.com>
2026-03-02 00:17:31 +0800

    -

--- backend-springboot/src/main/kotlin/com/yyjy/interceptor/TokenInterceptor.kt
+++ backend-springboot/src/main/kotlin/com/yyjy/interceptor/TokenInterceptor.kt
@@ -1,5 +1,6 @@
 package com.yyjy.interceptor
 
+import com.yyjy.common.BaseContext
 import com.yyjy.utils.JwtUtil
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.http.HttpServletRequest
