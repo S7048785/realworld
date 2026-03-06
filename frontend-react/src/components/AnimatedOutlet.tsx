b5d522d37ec388bbe6327a2d23648725dfe10d83
S7048785 <2889157408@qq.com>
2026-03-06 17:11:21 +0800

    -

--- frontend-react/src/components/AnimatedOutlet.tsx
+++ frontend-react/src/components/AnimatedOutlet.tsx
@@ -1,4 +1,4 @@
-import {useLocation, useOutlet} from "react-router-dom";
+import { useLocation, useOutlet } from "react-router-dom";
 import { motion, AnimatePresence } from "motion/react";
 
 function AnimatedOutlet() {
@@ -10,9 +10,9 @@ function AnimatedOutlet() {
       <AnimatePresence mode="wait">
         <motion.div
           key={location.pathname}
-          initial={{ y: 2, opacity: 0, filter : "blur(2px)" }}
-          animate={{ y: 0, opacity: 1, filter : "blur(0px)" }}
-          exit={{ y: 2, opacity: 0, filter : "blur(2px)" }}
+          initial={{ y: 2, opacity: 0, filter: "blur(2px)" }}
+          animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+          exit={{ y: 2, opacity: 0, filter: "blur(2px)" }}
           className="absolute inset-0 w-full "
         >
           {outlet}
